73a0c24e5db3f86ae81378e6bca1aa1e5ec6f537
SecDet Samurai <tylerblackout17@gmail.com>
2022-07-05 17:19:09 +0300

    ,

---
+++ src/base/services.py
@@ -0,0 +1,17 @@
+import os
+
+from django.core.validators import ValidationError
+
+
+def get_path_upload_product_image(instance, file):
+    """ Путь к обложке товара, format: (media)/products/product_id/photo.jpg """
+
+    return f"products/product_{instance.id}/{file}"
+
+
+def validate_size_image(file_object):
+    """ Проверка размера загружаемого файла """
+
+    megabyte_limit = 2
+    if file_object.size > megabyte_limit * 1024 * 1024:
+        raise ValidationError(f"Максимальный размер файла не должен превышать {megabyte_limit}MB")
